b8a1fdb2201eaed18734bad0a69a07804fa78f28
James Meyer <james.meyer@cognizant.com>
2020-03-14 16:39:44 -0500

    added readability

--- src/model.rs
+++ src/model.rs
@@ -2,15 +2,21 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Config {
+
     #[serde(rename = "jwtKeyName")]
     pub jwt_key_name: String,
+
     #[serde(rename = "jwtSecret")]
     pub jwt_secret: String,
+
     #[serde(rename = "maxConnection")]
     pub max_connection: usize,
+
     #[serde(rename = "maxRateOfConnection")]
     pub max_rate_of_connection: usize,
+
     pub timeout: usize,
+
     #[serde(rename = "maxPayloadSize")]
     pub max_payload_size: usize,
 }
