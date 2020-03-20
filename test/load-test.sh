b40cc329c1725ddd892103091cb7fe3a2a8cf7de
James Meyer <james.meyer@cognizant.com>
2020-03-20 13:34:10 -0500

    Updated load test to account for https implementation

--- test/load-test.sh
+++ test/load-test.sh
@@ -8,7 +8,7 @@ wait="$3"
 
 req(){
 	for i in $(seq 0 "$request_cout") ; do
-		curl -L -X GET 'http://127.0.0.1:8000/api/user' -w "%{http_code}\t%{time_total}\n" \
+		curl --insecure -L -X GET 'https://127.0.0.1:8000/api/user' -w "%{http_code}\t%{time_total}\n" \
 		  -H 'jwt-token:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjMyNTAzNjgwMDAwLCJncm91cHMiOlsidXNlcnMiXX0.q6lNQ2JaiStSgP7IRmJR8z237GaFD3MmN-ZNtcnpWa8'
     sleep "$wait"
 	done
