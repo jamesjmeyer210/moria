54c0de6eafb596cf604b8966aae159a4a5cdc84a
James Meyer <james.meyer@cognizant.com>
2020-03-30 20:57:29 -0500

    More refactoring with passing builds

--- src/url/dynamic_map.rs
+++ src/url/dynamic_map.rs
@@ -1,60 +1,81 @@
 use regex::Regex;
-use actix_web::http::Method;
-use crate::util::either::Either;
 
+use super::MetaData;
 
-
-struct UrlMap<'a> {
-    groups: Vec<String>,
-    origins: Vec<String>,
-    map: Vec<Vec<Either<String,Regex>>>,
-    search: Option<Vec<&'a str>>
+pub struct DynamicMap {
+    map: Vec<Vec<Regex>>,
+    meta: Vec<MetaData>,
 }
 
-impl <'a>UrlMap<'a> {
+impl DynamicMap {
 
-    fn find_in_vec(vec: &Vec<Either<String,Regex>>, target: &str) -> Option<usize> {
-        let mut i: usize = 0;
-        for either in vec {
-            match either {
-                // Either we can have a string of our url piece, like "/api" or "/users"
-                Either::This(a) => {
-                    if a == target {
-                        i = i + 1;
-                        return Some(i)
-                    }
-                },
-                // Or we have a regex because it is an optional type
-                Either::That(b) => {
-                    if b.captures(target).is_some() {
-                        i = i + 1;
-                        return Some(i)
-                    }
-                },
+    pub fn vec_contains(v: &Vec<Regex>, t: &str) -> bool {
+        for reg in v {
+            match reg.captures(t){
+                Some(t) => return true,
                 _ => ()
             }
         }
-        None
+        false
     }
 
-    fn get(&self, target: (usize,usize)) -> Either {
-        let index_of = match self.map.get(target.0) {
-            Either::This(a) => Either::This(a),
-            _ => Either::None,
-        };
-        Either::None
+    pub fn get(&self, target: &str) -> Option<&MetaData> {
+        self.meta.get(0)
     }
 
-    fn find(&self, taget: Vec<&str>) -> Vec<(usize,usize)> {
-        let mut x =  0;
-        let mut at: Vec<(usize,usize)> = Vec::new();
-
-        for i in self.map.iter() {
-            match UrlMap::find_in_vec(i, taget.get(i).unwrap()) {
-                Some(y) => at.push((x,y)),
-                _ => (),
-            }
-        }
-        at
-    }
 }
+
+// struct UrlMap<'a> {
+//     groups: Vec<String>,
+//     origins: Vec<String>,
+//     map: Vec<Vec<Either<String,Regex>>>,
+//     search: Option<Vec<&'a str>>
+// }
+//
+// impl <'a>UrlMap<'a> {
+//
+//     fn find_in_vec(vec: &Vec<Either<String,Regex>>, target: &str) -> Option<usize> {
+//         let mut i: usize = 0;
+//         for either in vec {
+//             match either {
+//                 // Either we can have a string of our url piece, like "/api" or "/users"
+//                 Either::This(a) => {
+//                     if a == target {
+//                         i = i + 1;
+//                         return Some(i)
+//                     }
+//                 },
+//                 // Or we have a regex because it is an optional type
+//                 Either::That(b) => {
+//                     if b.captures(target).is_some() {
+//                         i = i + 1;
+//                         return Some(i)
+//                     }
+//                 },
+//                 _ => ()
+//             }
+//         }
+//         None
+//     }
+//
+//     fn get<A,B>(&self, target: (usize,usize)) -> Either<A,B> {
+//         let index_of = match self.map.get(target.0) {
+//             Either::This(a) => Either::This(a),
+//             _ => Either::None,
+//         };
+//         Either::None
+//     }
+//
+//     fn find(&self, taget: Vec<&str>) -> Vec<(usize,usize)> {
+//         let mut x =  0;
+//         let mut at: Vec<(usize,usize)> = Vec::new();
+//
+//         for i in self.map.iter() {
+//             match UrlMap::find_in_vec(i, taget.get(i).unwrap()) {
+//                 Some(y) => at.push((x,y)),
+//                 _ => (),
+//             }
+//         }
+//         at
+//     }
+// }
--- src/url/mod.rs
+++ src/url/mod.rs
@@ -2,6 +2,23 @@ use serde::{Serialize, Deserialize};
 
 pub mod url_type;
 pub mod static_map;
+pub mod dynamic_map;
+
+use static_map::StaticMap;
+use dynamic_map::DynamicMap;
+
+#[derive(Serialize, Deserialize)]
+pub struct Endpoint {
+    pub path: String,
+    pub method: String,
+    pub groups: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Domain {
+    pub origin: String,
+    pub endpoints: Vec<Endpoint>,
+}
 
 #[derive(Clone)]
 pub struct MetaData {
@@ -18,15 +35,26 @@ impl MetaData {
     }
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct Endpoint {
-    pub path: String,
-    pub method: String,
-    pub groups: Vec<String>,
+pub struct UrlTree {
+    fixed: StaticMap,
+    dynamic: DynamicMap,
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct Domain {
-    pub origin: String,
-    pub endpoints: Vec<Endpoint>,
+impl UrlTree {
+
+    fn from_maps(s: StaticMap, u: DynamicMap) -> UrlTree {
+        UrlTree {
+            fixed: s,
+            dynamic: u,
+        }
+    }
+
+    pub fn try_fixed_get(&self, target: &str) -> Option<&MetaData> {
+        self.fixed.get(target)
+    }
+
+    pub fn try_dynamic_get(&self, target: &str) -> Option<&MetaData> {
+        self.dynamic.get(target)
+    }
+
 }
