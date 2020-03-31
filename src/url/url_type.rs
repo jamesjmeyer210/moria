74a370cba1dbe9312f34899b31c8618c117761bd
James Meyer <james.meyer@cognizant.com>
2020-03-31 15:13:07 -0500

    Added basic constructor tests

--- src/url/url_type.rs
+++ src/url/url_type.rs
@@ -29,29 +29,6 @@ impl UrlType {
             UrlType::String => r"/[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/",
         }
     }
-
-    // pub fn extract_string(s: &str) -> Option<UrlType> {
-    //     let reg_string: Regex = Regex::new(r"(S|s)+tring+\(+[1-9][0-9]{0,3}?\)").unwrap();
-    //     match reg_string.captures(s) {
-    //         Some(capture) => {
-    //             // Extract the selected text from captured value of the regex expression
-    //             let text: &str = capture.get(0).unwrap().as_str();
-    //             let reg_usize: Regex = Regex::new(r"[1-9][0-9]{0,3}").unwrap();
-    //
-    //             // The bellow unwrap errors are unreachable because we cannot enter this
-    //             // code block without already having a valid selection. The outer regex
-    //             // rules are more strict than the internal rules.
-    //             let str_usize = reg_usize.captures(text).unwrap().get(0).unwrap().as_str();
-    //
-    //             // A ParseIntError is unreachable in the blow line because we are extracting
-    //             // this usize after we've captured it in a regex expression.
-    //             let size = usize::from_str(str_usize).unwrap();
-    //
-    //             Some(UrlType::String(size))
-    //         },
-    //         _ => None
-    //     }
-    // }
 }
 
 #[cfg(test)]
@@ -62,8 +39,23 @@ mod test {
 
     #[test]
     fn from_str_returns_bool_when_given_bool() {
-        let t = UrlType::from_str("bool").unwrap();
+        let t = UrlType::from_str("{bool}").unwrap();
         assert_eq!(UrlType::Bool, t);
+    }
+
+    fn from_str_returns_integer_when_given_integer() {
+        let t = UrlType::from_str("{integer}").unwrap();
+        assert_eq!(UrlType::Integer, t);
+    }
+
+    fn from_str_returns_real_when_given_real() {
+        let t = UrlType::from_str("{real}").unwrap();
+        assert_eq!(UrlType::Real, t);
+    }
+
+    fn from_str_returns_string_when_given_string(){
+        let t = UrlType::from_str("{string}").unwrap();
+        assert_eq!(UrlType::String, t);
     }
 
     // #[test]
