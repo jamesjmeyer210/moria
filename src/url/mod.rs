b70b9654304b294fa2081a4885402e2dbe01ac6d
James Meyer <james.meyer@cognizant.com>
2020-03-31 14:45:09 -0500

    cleaned up url mod file

--- src/url/mod.rs
+++ src/url/mod.rs
@@ -1,11 +1,8 @@
 use serde::{Serialize, Deserialize};
 
+pub mod map;
 pub mod url_type;
 pub mod static_map;
-pub mod dynamic_map;
-
-use static_map::StaticMap;
-use dynamic_map::DynamicMap;
 
 #[derive(Serialize, Deserialize)]
 pub struct Endpoint {
@@ -33,28 +30,4 @@ impl MetaData {
             groups: g,
         }
     }
-}
-
-pub struct UrlTree {
-    fixed: StaticMap,
-    dynamic: DynamicMap,
-}
-
-impl UrlTree {
-
-    fn from_maps(s: StaticMap, u: DynamicMap) -> UrlTree {
-        UrlTree {
-            fixed: s,
-            dynamic: u,
-        }
-    }
-
-    pub fn try_fixed_get(&self, target: &str) -> Option<&MetaData> {
-        self.fixed.get(target)
-    }
-
-    pub fn try_dynamic_get(&self, target: &str) -> Option<&MetaData> {
-        self.dynamic.get(target)
-    }
-
 }
