cb1ff5cc07f3fb6ec7a07406f44eccbfd456facc
James Meyer <james.meyer@cognizant.com>
2020-03-31 14:33:57 -0500

    Added wrapper for Regex to implement the PartialEq trait

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,9 +1,24 @@
 #![macro_use]
 
 use std::borrow::Borrow;
+use regex::Regex;
 
 pub mod either;
 pub mod jwt;
+
+pub struct UrlRegex {
+    pub expr: Regex,
+}
+
+impl PartialEq for UrlRegex {
+    fn eq(&self, other: &Self) -> bool {
+        self.expr.as_str() == other.expr.as_str()
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        !self.eq(other)
+    }
+}
 
 // A wrapper for the Vec type that only accepts unique elements
 pub struct UniqueVec<T> {
