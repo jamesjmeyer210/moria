e59d637d412267b20201ca4ba02f57929f25d610
James Meyer <james.meyer@cognizant.com>
2020-04-01 10:05:45 -0500

    Added todo

--- src/util/url_regex.rs
+++ src/util/url_regex.rs
@@ -1,5 +1,8 @@
 use regex::Regex;
 
+// TODO:
+// This struct probably belong with the other url types and should possibly to private within the
+// url package - maybe it should be used underneath the url_type structure.
 pub struct UrlRegex {
     pub expr: Regex,
 }
