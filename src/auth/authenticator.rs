b92b4ab38c032df34f743b6d4ee8f4d55217e73f
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2021-01-16 14:29:56 -0600

    added tests for url_map

--- src/auth/authenticator.rs
+++ src/auth/authenticator.rs
@@ -1,6 +1,6 @@
 use crate::Config;
 use std::sync::{Arc, Mutex};
-use crate::auth::url_map::UrlMap;
+use crate::auth::url_map::UriMap;
 use actix_web::HttpRequest;
 use crate::auth::traits::Authentication;
 use actix_web::http::{HeaderMap, HeaderValue};
@@ -11,11 +11,11 @@ use actix_http::http::Uri;
 pub struct Authenticator {
     jwt_name: String,
     jwt_secret: String,
-    endpoints: UrlMap,
+    endpoints: UriMap,
 }
 
 impl Authenticator {
-    pub fn new(jwt_name: String, jwt_secret: String, endpoints: UrlMap) -> Self {
+    pub fn new(jwt_name: String, jwt_secret: String, endpoints: UriMap) -> Self {
         Authenticator {
             jwt_name,
             jwt_secret,
@@ -49,4 +49,36 @@ impl Authentication<&HttpRequest> for Authenticator {
             _ => false,
         }
     }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn verify_header_value_returns_true_when_value_is_valid() -> () {
+        let auth = Authenticator::new("".to_string()
+                                      , "secret".to_string()
+                                      , UriMap::new());
+
+        let token = HeaderValue::from_static("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9\
+        .eyJleHAiOjMyNTAzNjgwMDAwLCJncm91cHMiOlsidXNlcnMiLCJhZG1pbnMiXX0\
+        .8LGHRBirzKJPP4xhbyvIRLO-B7wMpUzJrOWgub4zASs");
+
+        assert!(auth.verify_header_value(&token));
+    }
+
+    #[test]
+    fn verify_header_value_returns_false_when_signature_does_not_match() -> () {
+        let auth = Authenticator::new("".to_string()
+                                      , "secret".to_string()
+                                      , UriMap::new());
+
+        let token = HeaderValue::from_static("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9\
+        .eyJleHAiOjMyNTAzNjgwMDAwLCJncm91cHMiOlsidXNlcnMiLCJhZG1pbnMiXX0\
+        .XO1xiMZljpvAOsXPGEKSJmyfgcUum7nOmUmw63kzyio");
+
+        assert!(!auth.verify_header_value(&token));
+    }
+
 }
--- src/auth/url_map.rs
+++ src/auth/url_map.rs
@@ -4,26 +4,53 @@ use crate::auth::traits::Authentication;
 use actix_http::http::Uri;
 use std::rc::Rc;
 
-pub struct UrlMap {
-    internal: HashMap<Rc<Uri>, AuthObj>,
+pub struct UriMap {
+    internal: HashMap<Uri, Vec<String>>,
 }
 
-impl UrlMap {
-    pub fn get(&self, url: &Uri) -> Option<&AuthObj> {
+impl UriMap {
+    pub fn new() -> Self {
+        UriMap {
+            internal: HashMap::new(),
+        }
+    }
+
+    pub fn get(&self, url: &Uri) -> Option<&Vec<String>> {
         self.internal.get(url)
     }
 }
 
-impl From<HashMap<Rc<Uri>, AuthObj>> for UrlMap {
-    fn from(src: HashMap<Rc<Uri>, AuthObj>) -> Self {
-        UrlMap {
+impl From<HashMap<Uri, Vec<String>>> for UriMap {
+    fn from(src: HashMap<Uri, Vec<String>>) -> Self {
+        UriMap {
             internal: src
         }
     }
 }
 
-impl Authentication<&Uri> for UrlMap {
-    fn authenticate(&self, uri: &Uri) -> bool {
-        self.internal.contains_key(uri)
+#[cfg(test)]
+mod test {
+    use crate::auth::url_map::UriMap;
+    use std::collections::HashMap;
+    use actix_http::http::Uri;
+
+    #[test]
+    fn get_returns_some_when_uri_is_match() -> () {
+        let mut hashmap = HashMap::with_capacity(1);
+        hashmap.insert(Uri::from_static("https://abc.api")
+                       , vec!["api_user".to_string(), "admin".to_string()]);
+
+        let uri_map = UriMap::from(hashmap);
+
+        let result = uri_map.get(&Uri::from_static("https://abc.api"));
+        assert!(result.is_some());
+        assert_eq!(&vec!["api_user".to_string(), "admin".to_string()], result.unwrap());
+    }
+
+    #[test]
+    fn get_returns_none_when_uri_is_mismatch() -> () {
+        let uri_map = UriMap::new();
+
+        assert!(uri_map.get(&Uri::from_static("https://abc.api")).is_none());
     }
 }
